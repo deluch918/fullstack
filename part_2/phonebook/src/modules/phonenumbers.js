c0d4144d576c1c805bab963119a5ee38e6f3d74e
Michael De Lucia <deluch918@gmail.com>
2025-05-14 12:17:07 -0500

    Updating phonebook app

--- part_2/phonebook/src/modules/phonenumbers.js
+++ part_2/phonebook/src/modules/phonenumbers.js
@@ -1,24 +1,24 @@
 import axios from "axios"
 
-const baseURL = "http://localhost:3001"
+const baseURL = "/api/persons"
 
 const getAll = () => {
-    const request = axios.get(`${baseURL}/api/persons`)
+    const request = axios.get(baseURL)
     return request.then((response) => response.data)
 }
 
 const create = (newObject) => {
-    const request = axios.post(`${baseURL}/api/persons`, newObject)
+    const request = axios.post(baseURL, newObject)
     return request.then((response) => response.data)
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseURL}/api/persons/${id}`, newObject)
+    const request = axios.put(`${baseURL}/${id}`, newObject)
     return request.then(response => response.data)
 }
 
 const deletePerson = (id) => {
-    const request = axios.delete(`${baseURL}/api/persons/${id}`)
+    const request = axios.delete(`${baseURL}/${id}`)
     return request.then(response => response.data)
 }
 
--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static('dist'))
 
 morgan.token("body", (req, res) => {
   if (req.method === "POST") {
@@ -124,7 +125,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
